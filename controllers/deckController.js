cb44edfc0beb35c1cb284e8b30cc956756911329
Bambo <phamtanbach123@gmail.com>
2025-11-25 15:46:03 +0700

    made deck controller functions

---
+++ controllers/deckController.js
@@ -0,0 +1,45 @@
+const Deck = require("../models/Deck");
+
+// Create a new deck
+async function createDeck(req, res) {
+  const { name, userID } = req.body;
+
+  if (!name || !userID)
+    return res.status(400).json({ message: "Missing name or UserID" });
+
+  try {
+    const deck = await Deck.create({ name, userID });
+    res.status(201).json(deck);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+}
+
+// Get all decks for a user
+async function getDeck(req, res) {
+  const { userID } = req.params;
+
+  try {
+    const deck = await Deck.find({ userID });
+    res.json(deck);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+}
+
+// Update deck
+async function updateDeck(req, res) {
+  const { deckID } = req.params;
+  const { name } = req.req;
+
+  try {
+    const deck = await Deck.findByIdAndUpdate(deckID, { name }, { new: true });
+    if (!deck) return res.status(404).json({ message: "Deck not found" });
+    res.json(deck);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+}
