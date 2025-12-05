20f95145952dae37a09072412204117b58146ae5
Bambo <phamtanbach123@gmail.com>
2025-12-05 18:17:04 +0700

    prepared for deployment

--- server.js
+++ server.js
@@ -38,7 +38,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "[https://your-frontend-app.vercel.app](https://your-frontend-app.vercel.app)",
+    ],
     credentials: true,
   })
 );
