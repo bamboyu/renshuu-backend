4e838f6391345c466d6b9af0d914bfb7fed51080
Bambo <phamtanbach123@gmail.com>
2025-12-07 19:33:21 +0700

    updated sm2

--- src/services/sm2.js
+++ src/services/sm2.js
@@ -3,31 +3,25 @@ function sm2(card, quality) {
 
   // quality: 0=Again, 1=Hard, 2=Good, 3=Easy
 
-  if (quality === 0) {
-    // If called, reset reps.
-    repetition = 0;
+  // Passing grades (1, 2, 3)
+  if (repetition === 0) {
     interval = 1;
+  } else if (repetition === 1) {
+    interval = 6;
   } else {
-    // Passing grades (1, 2, 3)
-    if (repetition === 0) {
-      interval = 1;
-    } else if (repetition === 1) {
-      interval = 6;
-    } else {
-      // Repetition >= 2: Apply modifiers based on rating
-      if (quality === 1) {
-        // Hard: 1.2x multiplier (minimum increase)
-        interval = Math.round(interval * 1.2);
-      } else if (quality === 2) {
-        // Good: Standard EF multiplier
-        interval = Math.round(interval * easeFactor);
-      } else if (quality === 3) {
-        // Easy: EF multiplier + Bonus (x1.3)
-        interval = Math.round(interval * easeFactor * 1.3);
-      }
+    // Repetition >= 2: Apply modifiers based on rating
+    if (quality === 1) {
+      // Hard: 1.2x multiplier (minimum increase)
+      interval = Math.round(interval * 1.2);
+    } else if (quality === 2) {
+      // Good: Standard EF multiplier
+      interval = Math.round(interval * easeFactor);
+    } else if (quality === 3) {
+      // Easy: EF multiplier + Bonus (x1.3)
+      interval = Math.round(interval * easeFactor * 1.3);
     }
-    repetition += 1;
   }
+  repetition += 1;
 
   // Ease Factor
   easeFactor =
